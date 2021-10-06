0d0aed5da267d68df1729175157f6f92ab6dd507
Olivera2708 <90214731+Olivera2708@users.noreply.github.com>
2021-10-06 18:13:54 +0200

    Add files via upload

--- Day26-NATOAlfabet/main.py
+++ Day26-NATOAlfabet/main.py
@@ -1,1 +1,22 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
 
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+data = pandas.read_csv("C:/Users/Olivera/Documents/Python learning/Day26-NATOAlfabet/nato_phonetic_alphabet.csv")
+phonetic_dic = {row.letter:row.code for (index, row) in data.iterrows()}
+
+def enter_word():
+    word = input("Eneter some word ").upper()
+    try:
+        result = [phonetic_dic[letter] for letter in word]
+    except KeyError:
+        print("Only letters please")
+        enter_word()
+    else:
+        print(result)
+
+enter_word()
