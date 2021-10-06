69114c9646e404a4c1364213da247591a068945c
Olivera2708 <90214731+Olivera2708@users.noreply.github.com>
2021-10-06 18:28:54 +0200

    Add files via upload

--- Day38-WorkoutTracker/main.py
+++ Day38-WorkoutTracker/main.py
@@ -1,1 +1,57 @@
+import requests
+from datetime import datetime
 
+API_KEY = "bb93c7e10f9b9d4d35eece441a4606da"
+API_ID = "2eb9de89"
+
+GENDER = "Female"
+WEIGHT = 65
+HEIGHT = 170
+AGE = 19
+
+endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+api_sheety = "https://api.sheety.co/61fe0298575e162c657cddd733dd40ba/workout/лист1"
+
+hearder = {
+    "x-app-id": API_ID,
+    "x-app-key": API_KEY,
+}
+
+sentence = input("Tell me what you did today -> ")
+
+params = {
+    "query": sentence,
+    "gender": GENDER,
+    "weight_kg": WEIGHT,
+    "height_cm": HEIGHT,
+    "age": AGE
+}
+
+response = requests.post(url=endpoint, json=params, headers=hearder)
+result = response.json()
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_input = {
+        "лист1": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+bearer_headers = {
+    "Authorization": "Bearer 123123123"
+}
+sheet_response = requests.post(
+    api_sheety, 
+    json=sentence, 
+    headers=bearer_headers
+)
+
+response = requests.post(api_sheety, json=sheet_input)
+print(response.text)
