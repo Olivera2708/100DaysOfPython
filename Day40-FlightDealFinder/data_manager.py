59d7c821638369bac6e155075d1478bb51f9a945
Olivera2708 <90214731+Olivera2708@users.noreply.github.com>
2021-10-06 18:29:56 +0200

    Add files via upload

---
+++ Day40-FlightDealFinder/data_manager.py
@@ -0,0 +1,31 @@
+import requests
+
+sheety_api = "https://api.sheety.co/61fe0298575e162c657cddd733dd40ba/flight/prices"
+
+class DataManager:
+    def __init__(self):
+         self.destination_data = {}
+
+    def get_setination_data(self):
+        response = requests.get(url=sheety_api)
+        data = response.json()
+        self.destination_data = data["prices"]
+
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "prices": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(url=f"{sheety_api}/{city['id']}", json=new_data)
+            print(response.text)
+
+    def get_customer_emails(self):
+        customers_endpoint = ""
+        response = requests.get(customers_endpoint)
+        data = response.json()
+        self.customer_data = data["users"]
+        return self.customer_data
