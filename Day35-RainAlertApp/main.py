fcd899a5fe60d7c534970c035d688379c9de49d6
Olivera Radovanovic <90214731+Olivera2708@users.noreply.github.com>
2021-10-10 20:56:54 +0200

    Update main.py

--- Day35-RainAlertApp/main.py
+++ Day35-RainAlertApp/main.py
@@ -1,8 +1,8 @@
 import requests
 
-API_KEY = "69f04e4613056b159c2761a9d9e664d2"
-LAT = 45.252359
-LNG = 19.791228
+API_KEY = ""
+LAT = 
+LNG = 1
 
 response = requests.get(url=f"https://api.openweathermap.org/data/2.5/onecall?lat={LAT}&lon={LNG}&exclude=current,daily,minutely&appid={API_KEY}")
 response.raise_for_status()
