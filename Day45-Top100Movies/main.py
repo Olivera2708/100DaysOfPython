688c0d19dbd1eb32203d02f3c971905d88377208
Olivera Radovanovic <90214731+Olivera2708@users.noreply.github.com>
2021-10-10 20:51:48 +0200

    Add files via upload

--- Day45-Top100Movies/main.py
+++ Day45-Top100Movies/main.py
@@ -1,1 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
 
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+all_movies = soup.findAll(name="h3", class_ = "title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+
+movies = movie_titles[::-1]
+
+print(all_movies)
+
+with open("Day45-Top100Movies/movies.txt", "w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
