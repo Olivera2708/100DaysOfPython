cf34072cd53bdc8c6c39a6d8237ef41d213c2adb
Olivera Radovanovic <90214731+Olivera2708@users.noreply.github.com>
2021-10-10 20:59:41 +0200

    Add files via upload

---
+++ Day33-ISSTracker/main.py
@@ -0,0 +1,52 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_LAT = 45.2471933 # Your latitude
+MY_LONG = 19.7937028 # Your longitude
+
+MY_EMAIL = "olivera.radovanovic2708@gmail.com"
+MY_PASSWORD = "olivera2002"
+
+#Your position is within +5 or -5 degrees of the ISS position.
+def is_within():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    return MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5
+
+
+def is_dark():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now()
+    return sunset <= time_now.hour <= sunrise
+
+def send_mail():
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs="olivera.radovanovic2002@gmail.com",
+            msg=f"Subject:Look above!\n\nISS Above you!!!"
+        )
+
+while True:
+    time.sleep(60)
+    if is_dark() and is_within():
+        send_mail()
