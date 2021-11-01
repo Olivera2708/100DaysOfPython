65e34ef8a9700b3a2a2fcdbb4fac492a1e709a29
Olivera Radovanovic <90214731+Olivera2708@users.noreply.github.com>
2021-11-01 15:32:08 +0100

    Add files via upload

--- Day59-BlogUpdate/main.py
+++ Day59-BlogUpdate/main.py
@@ -1,1 +1,40 @@
+from flask import Flask, render_template
+import requests
+from post import Post
 
+response = requests.get("https://api.npoint.io/88c2c1f644ef334058be")
+posts = response.json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["date"], post["author"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
+
+@app.route('/index.html')
+def home():
+    return render_template("index.html", all_posts=post_objects)
+
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/post.html/<int:index>')
+def post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
